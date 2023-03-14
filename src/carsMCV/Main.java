ffd1332ed131ef8987d68c34a3c58df0f1e631de
ENE MIHAELA <mihaela.een@gmail.com>
2023-03-14 12:31:00 +0200

    finish

--- src/carsMCV/Main.java
+++ src/carsMCV/Main.java
@@ -1,8 +1,12 @@
 package carsMCV;
 
+import carsMCV.view.View;
+
 public class Main {
     public static void main(String[] args) {
 
+        View view = new View();
+        view.play();
 
     }
 }
